ccb3190b27f99d495a4fbe81aea448e33e10cb36
Santhosh <santhoosh@vmi481301.contaboserver.net>
2020-12-15 19:42:57 +0530

    m30s: use aosp_base for android-10

--- omni_m30s.mk
+++ omni_m30s.mk
@@ -5,13 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from m30s device
-$(call inherit-product, device/samsung/m30s/device.mk)
+$(call inherit-product, build/target/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
